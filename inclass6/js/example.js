3e8b163f0971d3c58fda869d1d4a7fdbfdd23c42
Jequatot <jeffreyquattro@gmail.com>
2018-11-08 10:22:19 -0500

    inclass6

---
+++ inclass6/js/example.js
@@ -0,0 +1,20 @@
+var list = document.getElementById('one').parentNode;
+
+// ADD NEW ITEM TO END OF LIST
+var newitem = document.createElement('li');
+newitem.innerHTML = 'cream';
+list.appendChild(newitem);
+
+// ADD NEW ITEM START OF LIST
+var newitem = document.createElement('li');
+newitem.innerHTML = 'kale';
+list.insertBefore(newitem, document.getElementById('one'));
+
+// ADD A CLASS OF COOL TO ALL LIST ITEMS
+var items = document.getElementsByTagName('li');
+for(i = 0; i < items.length; i++) {
+	items[i].className = "cool";
+}
+
+// ADD NUMBER OF ITEMS IN THE LIST TO THE HEADING
+document.getElementById("header").innerHTML += ": " + items.length + " items";
